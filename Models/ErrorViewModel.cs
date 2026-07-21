ab8ffa48404164822384cce31195c8bcc0d4b776
NS1Beast <fvgfv123nam@gmail.com>
2026-07-21 09:33:01 +0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebMTTQ.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
