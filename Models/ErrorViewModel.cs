650727c8fdd3e0b4f26814ddbd0d6946c6a7a5c5
NS1Beast <fvgfv123nam@gmail.com>
2026-07-21 09:34:05 +0700

    init

--- Models/ErrorViewModel.cs
+++ Models/ErrorViewModel.cs
@@ -3,7 +3,7 @@ namespace WebMTTQ.Models
     public class ErrorViewModel
     {
         public string? RequestId { get; set; }
-
+        //test
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
 }
